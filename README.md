c56c2b41c7e2b26982a3f2a5fa92a056ce40956b
MaryannWanguiMuhindi <maryannmuhindi@gmail.com>
2024-07-11 13:59:36 +0300

    Add Wk-1-code challenge solution1.js

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Wk-1-Code-challenge
+
+This repository contains the solutions for the week 1 code challenge.
+
+## solutions
+
+1. "solution 1"
+
---
+++ solution2.js
@@ -0,0 +1,8 @@
+function checkspeed() {
+    let speed = prompt("Enter car speed");
+    speed = parseInt(speed);
+
+    if (speed < 70){
+        console.log("Ok");
+    } else 
+}
